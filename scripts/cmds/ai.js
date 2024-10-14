d3531153b25231730e27b0c456b272c57f6cf701
Orochimaruose <createdeno61@gmail.com>
2024-10-14 18:48:50 +0100

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -19,7 +19,7 @@ function formatFont(text) {
   };
 
 
-  let formattedText = "";
+  let formattedText = "🤾";
 
   for (const char of text) {
 
@@ -99,14 +99,14 @@ module.exports = {
 
       if (!np) {
 
-        return api.sendMessage('💬 Que puis-je faire pour toi 🧘🏽‍♂️', event.threadID, event.messageID);
+        return api.sendMessage('🤾', event.threadID, event.messageID);
 
       }
 
 
       const cliff = await new Promise(resolve => {
 
-        api.sendMessage('[♻️] Please Wait....', event.threadID, (err, info1) => {
+        api.sendMessage('[🤾] ✨ Attend un peu 💁👻....', event.threadID, (err, info1) => {
 
           resolve(info1);
 
@@ -131,7 +131,7 @@ module.exports = {
 
             role: "user",
 
-            content: `💬 𝙃𝙞 𝙄'𝙢 𝙈𝙞𝙖 𝙍𝙤𝙣𝙖𝙡𝙙'𝙨 𝙫𝙞𝙧𝙩𝙪𝙖𝙡 𝙖𝙨𝙨𝙞𝙨𝙩𝙖𝙣𝙩. 𝙒𝙝𝙖𝙩 𝙘𝙖𝙣 𝙞 𝙙𝙤 𝙛𝙤𝙧 𝙮𝙤𝙪 🤓?`
+            content: `🤾?`
 
           },
 
