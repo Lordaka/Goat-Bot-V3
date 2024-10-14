1047e9336f399b915e5e28a5c764236921e17615
Orochimaruose <createdeno61@gmail.com>
2024-10-14 18:42:44 +0100

    Update upt.js

--- scripts/cmds/upt.js
+++ scripts/cmds/upt.js
@@ -1,9 +1,9 @@
- module.exports = {
+module.exports = {
   config: {
-    name: "upt",
-    aliases: ["up"],
+    name: "uptime",
+aliases: ["upt"],
     version: "1.0",
-    author: "Ronald",
+    author: "OtinXSandip",
     role: 2,
     shortDescription: {
       en: "Displays the total number of users of the bot and check uptime "
@@ -21,21 +21,18 @@
       const allUsers = await usersData.getAll();
       const allThreads = await threadsData.getAll();
       const uptime = process.uptime();
-      
-const days = 
+   const days = 
 Math.floor(uptime / (3600 * 24));
       const hours = Math.floor(uptime / 3600);
       const minutes = Math.floor((uptime % 3600) / 60);
       const seconds = Math.floor(uptime % 60);
       
-      const uptimeString = `${days} : ${hours} : ${minutes} : ${seconds}`;
+      const uptimeString = `${days}𝙹𝙾𝚄𝚁𝚂☩ ${hours}𝙷𝚁𝚂☩ ${minutes}𝙼𝙸𝙽☩ ${seconds}𝚂𝙴𝙲☩`;
       
-      api.sendMessage(`[⌛] 𝙏𝙞𝙢𝙚\n\n✪➩ ${uptimeString}\n
-[👨🏽‍🦱] 𝙉𝙪𝙢𝙗𝙚𝙧 𝙛𝙤𝙧 𝙪𝙨𝙚𝙧𝙨\n\n✪➩ ${allUsers.length}\n
-[💬] 𝙉𝙪𝙢𝙗𝙚𝙧 𝙛𝙤𝙧 𝙙𝙞𝙨𝙘𝙪𝙨𝙨𝙞𝙤𝙣\n\n✪➩ ${allThreads.length}`, event.threadID);
+      api.sendMessage(`✨| ✰.....𝗗𝗨𝗥𝗘𝗘....✰\n➫ ${uptimeString}\n\n👻 | 𝗨𝗧𝗜𝗟𝗜𝗦𝗔𝗧𝗘𝗨𝗥𝗦🧑‍💻\n➫ ${allUsers.length}\n🕊| 𝗚𝗥𝗢𝗨𝗣𝗘+𝗣𝗩🙌\n➫ ${allThreads.length}`, event.threadID);
     } catch (error) {
       console.error(error);
-      api.sendMessage("An error occurred while retrieving data.", event.threadID);
+      api.sendMessage("https://i.ibb.co/94GF03N/image.jpg", event.threadID);
     }
   }
 };
